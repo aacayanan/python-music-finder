5c1573353d6cc56c24d89798f8fbdcaf146081e7
Aaron Cayanan <aaroncayanan02@gmail.com>
2024-12-05 19:14:30 -0800

    initial commit, create WindowBase class, create LoginPage

---
+++ window_base.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+class BaseWindow:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.title = "Music Finder"
+        self.width = 480
+        self.height = 240
+        self.setup_window()
+        self.root.resizable(False, False)
+
+    def setup_window(self):
+        self.root.title(self.title)
+        self.root.geometry(f'{self.width}x{self.height}')
+
+    def add_component(self, component):
+        component.pack()
+
+    def show(self):
+        self.root.mainloop()
